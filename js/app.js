04e45a40a314439377559fc87fa6b3d57c2a8884
Daniel Rosenthal <dan_rosenthal@mail.msj.edu>
2013-03-08 14:27:14 -0500

    Set up a JS weather app

--- js/app.js
+++ js/app.js
@@ -1,26 +1,12 @@
-// options :
-var my_city="45233";
-var my_key="5b0bef4cc7145805130803";
-var no_of_days=2;
-// build URI:
-var uri="http://free.worldweatheronline.com/feed/weather.ashx?q="+my_city+"&key="+my_key+"&format=json&no_of_days="+no_of_days+"&includeLocation=yes";
-// uri-encode it to prevent errors :
-uri=encodeURI(uri); 
+$(document).ready(function () {
+	$.getJSON('http://free.worldweatheronline.com/feed/weather.ashx?q=45233&format=json&num_of_days=2&key=5b0bef4cc7145805130803&callback=?', 
+	function(json) {
+		var currentCondition = json.data.current_condition[0].weatherDesc[0].value;
+    	var conditionClass = currentCondition.replace(/\s/g, "");
+    	var currentTemp = json.data.current_condition[0].temp_F;
 
-jQuery.get(uri,function(r){
-
- if(r.data.error) {
-
-  // error exists, so display it
-  alert(r.data.error[0].msg);
-
-  } else {
-
-   // no error
-   // add some weather data to variables
-   var current_condition = r.data.current_condition[0].weatherDesc[0].value;
-   alert(current_condition);
-
- }//end if
-
-},"jsonp"); // end jQuery.get()
+		$("span.condition").addClass(conditionClass);
+		$("span.condition").text(currentCondition);
+		$("span.temperature").text(currentTemp);
+	});
+});
